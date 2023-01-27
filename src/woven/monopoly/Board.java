4a26444f7cb516c5b821b8d444c20b1aa390a2c2
Ehsan Khalid <s3838975@student.rmit.edu.au>
2023-01-27 20:12:14 +1100

    Added the loadBoard method in Board class which loads all the properties from board.json and parses them into the Property object

--- src/woven/monopoly/Board.java
+++ src/woven/monopoly/Board.java
@@ -12,15 +12,22 @@ import org.json.simple.parser.ParseException;
 
 public class Board {
 
+    // This method loads the Monopoly Board from the board.json file
     public List<Property> loadBoard() {
+
+        // Creating a new instance of an object
         List<Property> properties = new ArrayList<Property>();
         JSONParser parser = new JSONParser();
 
         try {
+            // Reads the JSON file and then uses the parser to parse it and assigns
+            // it to jsonProperties and casts is as an JSON Array
             JSONArray jsonProperties = (JSONArray) parser.parse(new FileReader("board.json"));
 
+            // Loops over the jsonProperties and gets each element and adds it to the property list
             for (Object obj : jsonProperties) {
                 JSONObject jsonProp = (JSONObject) obj;
+
                 String name = (String) jsonProp.get("name");
                 int price = ((Long) jsonProp.get("price")).intValue();
                 String colour = (String) jsonProp.get("colour");
@@ -32,6 +39,7 @@ public class Board {
             e.printStackTrace();
         }
 
+        // Returns the list of properties
         return properties;
     }
 }
--- src/woven/monopoly/Game.java
+++ src/woven/monopoly/Game.java
@@ -1,5 +1,24 @@
 package woven.monopoly;
 
+import java.util.List;
+
 public class Game {
 
+    static List<Player> players;
+    static List<Property> properties;
+    static List<Integer> rolls;
+    static int currentPlayerIndex;
+
+    public void startGame() {
+
+        // Create a new Board object
+        Board board = new Board();
+
+        System.out.println("Welcome to Woven Monopoly!");
+        System.out.println();
+
+        // load board from board.json and add the properties on the board to this variable
+        properties = board.loadBoard();
+
+    }
 }
