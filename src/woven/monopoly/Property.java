8bc42642b0a01aa9f13e986a38e705f980fea9dc
Ehsan Khalid <s3838975@student.rmit.edu.au>
2023-01-27 18:44:10 +1100

    Added all properties for the Property Class that makes up a property

--- src/woven/monopoly/Property.java
+++ src/woven/monopoly/Property.java
@@ -2,5 +2,43 @@ package woven.monopoly;
 
 public class Property {
 
+    private String name;
+    private int price;
+    private String colour;
+    private String type;
+    private int rent;
+    private Player owner;
 
+    private Property(String name, int price, String colour, String type, int rent){
+        this.name = name;
+        this.price = price;
+        this.colour = colour;
+        this.type = type;
+        this.rent = rent;
+        this.owner = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
 }
