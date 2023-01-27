92523668482336a9cb49b6dc796f0fa446cc0f11
Ehsan Khalid <s3838975@student.rmit.edu.au>
2023-01-27 19:12:01 +1100

    Added Pay Rent fucntionality for players who land on properties purchased by other player, and rent is doubled if the owner owns all property of the same colour, the money is also subtracted and added to the owner

--- src/woven/monopoly/Player.java
+++ src/woven/monopoly/Player.java
@@ -20,11 +20,27 @@ public class Player {
         this.ownedProperties = new ArrayList<Property>();
     }
 
+    // This allows player to buy property if it is available and then subtracts
+    // the property amount from their balance and also the player now owns it
     public void buyProperty(Property property){
         if (property.isAvailable() && this.money >= property.getPrice()){
             this.money -= property.getPrice();
             this.ownedProperties.add(property);
             property.setOwner(this);
+        }
+    }
+
+    // If the property player lands on is not owned by them then they pay rent
+    // and if the owner of the property owns all the same coloured property the
+    // rent is doubled
+    public void payRent(Property property){
+        if (!property.isAvailable() && property.getOwner() != this){
+            int rent = property.getRent();
+            if (property.isMonopoly()){
+                rent *= 2;
+            }
+            this.money -= rent;
+            property.getOwner().money += rent;
         }
     }
 
